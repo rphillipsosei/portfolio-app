814634cc22f8618eede807d95f85162787c45c97
Rhoda P <rhodaphillipsosei@gmail.com>
2022-05-09 15:04:07 -0400

    Add tooltips to nav

--- components/Nav.js
+++ components/Nav.js
@@ -3,23 +3,27 @@ import { Nav } from "react-bootstrap/Nav";
 import { AiFillGithub, AiFillLinkedin, AiFillHome } from "react-icons/Ai";
 import { MdEmail } from "react-icons/Md";
 import Link from "next/link";
-import { motion } from "framer-motion"
-
+import { motion } from "framer-motion";
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
 
 export default Nav = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link href="/">
-          <motion.a
-          whileHover={{
-            scale: 1.1,
-          }}
-          whileTap={{ scale: 1.1 }}
-          id="homelink">
-            <AiFillHome />
-          </motion.a>
-        </Link>
+        
+          <Link href="/">
+           <motion.a
+              whileHover={{
+                scale: 1.1,
+              }}
+              whileTap={{ scale: 1.1 }}
+              id="homelink"
+            >
+              <AiFillHome />
+            </motion.a>
+          </Link>
+        
         <div className="container-fluid">
           <button
             className="navbar-toggler"
@@ -35,34 +39,37 @@ export default Nav = () => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <div id="social-icons">
               <Link href="https://linkedin.com/in/rphillipsosei">
-                <motion.a 
-                    whileHover={{
-                      scale: 1.1,
-                    }}
-                    whileTap={{ scale: 1.1 }}
-                id="navlink">
+                 <Tippy content='LinkedIn'><motion.a
+                  whileHover={{
+                    scale: 1.1,
+                  }}
+                  whileTap={{ scale: 1.1 }}
+                  id="navlink"
+                >
                   <AiFillLinkedin />
-                </motion.a>
+                </motion.a></Tippy>
               </Link>
               <Link href="https://github.com/rphillipsosei">
-              <motion.a 
-                    whileHover={{
-                      scale: 1.1,
-                    }}
-                    whileTap={{ scale: 1.1 }}
-                     id="navlink">
+              <Tippy content='Github'><motion.a
+                  whileHover={{
+                    scale: 1.1,
+                  }}
+                  whileTap={{ scale: 1.1 }}
+                  id="navlink"
+                >
                   <AiFillGithub />
-                </motion.a>
+                </motion.a></Tippy>
               </Link>
               <Link href="/contact">
-                <motion.a
-                whileHover={{
-                  scale: 1.1,
-                }}
-                whileTap={{ scale: 1.1 }}
-                id="navlink">
+              <Tippy content='Get In Touch'><motion.a
+                  whileHover={{
+                    scale: 1.1,
+                  }}
+                  whileTap={{ scale: 1.1 }}
+                  id="navlink"
+                >
                   <MdEmail />
-                </motion.a>
+                </motion.a></Tippy>
               </Link>
             </div>
           </div>
